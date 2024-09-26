c1fb7d0dbcadd9f01fca1ee97d806659a3b8c10a
Rupam Bhakta <109302173+rupambhakta@users.noreply.github.com>
2024-09-27 02:51:38 +0530

    conveyorBeltProblem

---
+++ conveyorBeltProblem.java
@@ -0,0 +1,33 @@
+/*
+Problem Statement –
+
+A chocolate factory is packing chocolates into the packets. The chocolate packets here represent an array  of N number of integer values. The task is to find the empty packets(0) of chocolate and push it to the end of the conveyor belt(array).
+
+Example 1 :
+
+N=8 and arr = [4,5,0,1,9,0,5,0].
+
+There are 3 empty packets in the given set. These 3 empty packets represented as O should be pushed towards the end of the array
+*/ 
+
+public class conveyorBeltProblem{
+    public static void main(String args[]){
+        int arr[] = {4,5,0,1,9,0,5,0};
+        int n = arr.length;
+        int res[] = new int[n];
+        int first = 0;
+        int last = n-1;
+        for(int i=0; i<n; i++){
+            if(arr[i]==0){
+                res[last] = arr[i];
+                last--;
+            }else{
+                res[first] = arr[i];
+                first++;
+            }
+        }
+        for(int i : res){
+            System.out.print(i+" ");
+        }
+    }
+}
