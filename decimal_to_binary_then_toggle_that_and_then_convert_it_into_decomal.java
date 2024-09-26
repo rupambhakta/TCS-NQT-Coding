9a81318e11cdfae3dc6f3596259aec18f9feebbd
Rupam Bhakta <109302173+rupambhakta@users.noreply.github.com>
2024-09-27 03:07:04 +0530

    decimal_to_binary_then_toggle_that_and_then_convert_it_into_decomal

--- decimal_to_binary_then_toggle_that_and_then_convert_it_into_decomal.java
+++ decimal_to_binary_then_toggle_that_and_then_convert_it_into_decomal.java
@@ -1,6 +1,7 @@
 import java.util.*;
-public class decimal_to_binary_then_toggle_that_and_then_convert_it_into_decomal{
-    public static void main(String args[]){
+
+public class decimal_to_binary_then_toggle_that_and_then_convert_it_into_decomal {
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a decimal value: ");
         int n = sc.nextInt();
@@ -12,49 +13,52 @@ public class decimal_to_binary_then_toggle_that_and_then_convert_it_into_decomal
         System.out.println(ans);
 
     }
-    public static int decomalToBinary(int n){
+
+    public static int decomalToBinary(int n) {
         ArrayList<Integer> arr = new ArrayList<>();
         StringBuilder st = new StringBuilder();
-        while(n!=0){
+        while (n != 0) {
             // System.out.print(n%2+" ");
-            arr.add(n%2);
-            n = n/2;
+            arr.add(n % 2);
+            n = n / 2;
         }
         // System.out.println();
-        for(int i = arr.size()-1; i>=0; i--){
+        for (int i = arr.size() - 1; i >= 0; i--) {
             st.append(arr.get(i));
         }
         return Integer.parseInt(st.toString());
     }
-    //Alter value and most significant bit
-    public static int alterBinary(int b){
+
+    // Alter value and most significant bit
+    public static int alterBinary(int b) {
         int length = String.valueOf(b).length();
         int arr[] = new int[length];
-        for(int i = length-1; i > 0 ;i--){
-            if(b%10 == 0){
+        for (int i = length - 1; i > 0; i--) {
+            if (b % 10 == 0) {
                 arr[i] = 1;
-            }else{
+            } else {
                 arr[i] = 0;
             }
-            b = b/10;
+            b = b / 10;
         }
-        StringBuilder st  = new StringBuilder();
+        StringBuilder st = new StringBuilder();
         int x = 0;
-        while(arr[x] == 0){
+        while (arr[x] == 0) {
             x++;
         }
-        for(int i = x; i < length ;i++){
+        for (int i = x; i < length; i++) {
             st.append(arr[i]);
         }
         return Integer.parseInt(st.toString());
     }
-    public static int binaryToDecimal(int n){
+
+    public static int binaryToDecimal(int n) {
         int length = String.valueOf(n).length();
         int sum = 0;
-        for(int i = 0; i< length; i++){
-            int lb = n%10;
+        for (int i = 0; i < length; i++) {
+            int lb = n % 10;
             sum += lb * Math.pow(2, i);
-            n = n/10;
+            n = n / 10;
         }
         return sum;
     }
